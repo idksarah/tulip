e235805215a6a1e8ccaa5305e5f1060fab243321
idksarah <sarah.p.ngai@gmail.com>
2024-10-12 18:36:33 -0700

    actualalfel

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const htmlElement = document.getElementsByTagName("html")[0];
+let currentZoom = 100;
+
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
   function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
   return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,6 +10,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
       step((generator = generator.apply(thisArg, _arguments || [])).next());
   });
 };
+
 var __generator = (this && this.__generator) || function (thisArg, body) {
   var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
   return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
@@ -24,7 +28,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
               default:
                   if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
                   if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                  if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                  if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; break; }
                   if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
                   if (t[2]) _.ops.pop();
                   _.trys.pop(); continue;
@@ -34,84 +38,108 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
       if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
   }
 };
-var _this = this;
-var timerStart = document.querySelector(".timerStart");
+
+const timerStart = document.querySelector(".timerStart");
+
 function playTimer() {
-  timerStart.style.backgroundColor = "#BE4B35";
-  timerStart.style.color = "#EEBF7C";
+  timerStart.style.backgroundColor = "#dd9f9b";
+  timerStart.style.color = "#FFFFFF";
   timerStart.textContent = "stop";
-  var timerCountdown = document.querySelector(".timerCountdown");
+  const timerCountdown = document.querySelector(".timerCountdown");
   if (timerCountdown) {
-      var time = timerCountdown.textContent;
-      var a = (time === null || time === void 0 ? void 0 : time.split(':')) || ['0', '0'];
-      var minutes = (+a[0]);
-      var seconds = (+a[1]);
-      var totalSeconds_1 = minutes * 60 + seconds;
-      var myVar = setInterval(function () { myTimer(); }, 1000);
-      function myTimer() {
-          if (totalSeconds_1 <= 0) {
-              myStopFunction();
-          }
-          var displayMinutes = Math.floor(totalSeconds_1 / 60);
-          var displaySeconds = totalSeconds_1 % 60;
-          timerCountdown.innerHTML = zeroPad(displayMinutes, 2) + ':' + zeroPad(displaySeconds, 2);
-          totalSeconds_1 = totalSeconds_1 - 1;
+    let time = timerCountdown.textContent;
+    let a = time?.split(':') || ['0', '0'];
+    let minutes = (+a[0]);
+    let seconds = (+a[1]);
+    let totalSeconds = minutes * 60 + seconds;
+    let myVar = setInterval(() => myTimer(), 1000);
+    
+    function myTimer() {
+      if (totalSeconds <= 0) {
+        myStopFunction();
       }
-      function myStopFunction() {
-          clearInterval(myVar);
-      }
-      function zeroPad(num, places) {
-          var zero = places - num.toString().length + 1;
-          return Array(+(zero > 0 && zero)).join("0") + num;
-      }
+      let displayMinutes = Math.floor(totalSeconds / 60);
+      let displaySeconds = totalSeconds % 60;
+      timerCountdown.innerHTML = zeroPad(displayMinutes, 2) + ':' + zeroPad(displaySeconds, 2);
+      totalSeconds--;
+    }
+    
+    function myStopFunction() {
+      clearInterval(myVar);
+    }
+    
+    function zeroPad(num, places) {
+      let zero = places - num.toString().length + 1;
+      return Array(+(zero > 0 && zero)).join("0") + num;
+    }
   }
 }
-function hideElement(elementName){
-    elementName.classList.add("hidden");
+
+function hideElement(elementName) {
+  elementName.classList.add("hidden");
 }
 
 timerStart.addEventListener("click", () => playTimer());
 
 const timerExit = document.querySelector(".timerExit");
 timerExit.addEventListener("click", () => {
-    const timer = document.querySelector(".timer");
-    hideElement(timer);
-})
+  const timer = document.querySelector(".timer");
+  hideElement(timer);
+});
 
-const video = document.getElementById('video')
+const video = document.getElementById('video');
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
+]).then(startVideo);
+
+let currentExpression = '';
 
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
     stream => video.srcObject = stream,
     err => console.error(err)
-  )
+  );
 }
+
 video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  
+  const canvas = faceapi.createCanvasFromMedia(video);
   canvas.style.position = 'absolute';
-  canvas.style.left = '11em';    
-  canvas.style.top = '26em';
-  
-  document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
+  canvas.style.left = '11em';
+  canvas.style.top = '24em';
+  canvas.classList.add("hidden"); //unhide this for demo
+  document.body.append(canvas);
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
 
   setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
-})
-  
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+    
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    
+    if (resizedDetections.length > 0) {
+      const expressions = resizedDetections[0].expressions; 
+      const dominantExpression = Object.keys(expressions).reduce((a, b) => expressions[a] > expressions[b] ? a : b); 
+      
+      console.log(`Current Expression: ${dominantExpression}`);
+      currentExpression = dominantExpression; 
+    }
+
+    if (currentExpression === "sad" || currentExpression === "happy") {
+      if(currentZoom!=200){
+        currentZoom += 50; 
+        htmlElement.style.fontSize = currentZoom + "%";
+      }
+    }
+  }, 450);
+});
+
+startVideo();
